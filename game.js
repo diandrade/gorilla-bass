2db33cf4095052914e45f7b7eb500347a0c39678
diandrade <contato.andradediego@gmail.com>
2025-05-23 15:14:22 -0300

    Seleção das primeiras imagens dentro do projeto.

---
+++ game.js
@@ -0,0 +1,9 @@
+//load images
+let gorillaImage = new Image();
+gorillaImage.src = './assets/gorilla.png';
+
+let humanImage = new Image();
+humanImage.src = './assets/human.png';
+
+let arenaImage = new Image();
+arenaImage.src = './assets/arena.png';
