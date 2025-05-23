244a093ca7b85e1b9d29304275696ae664fad088
diandrade <contato.andradediego@gmail.com>
2025-05-23 16:11:30 -0300

    Adição do background do canvas, com o pântano.

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
-const canvas = document.getElementById('gameCanvas');
+const canvas = document.querySelector('#game-canvas');
+const startBtn = document.querySelector('#start-button');
 const ctx = canvas.getContext('2d');
-
-canvas.style.background = "#ff8";
 
 //Função responsável por adaptar o canvas do JS dentro do espaço da tela, de maneira responsiva.
 function resizeCanvas() {
